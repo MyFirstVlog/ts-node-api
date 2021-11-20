24006bef03c98971c7f56a075dcc04cf509a7a71
Alejandro Estrada <alejandro.estradam@udea.edu.co>
2021-11-20 14:52:36 -0500

    express api, ts-node beginnings

---
+++ express-api/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+// const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+
+  res.status(201).json({
+      ok:true,
+      msg:'todo correcto'
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
